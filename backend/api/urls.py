383ff68d8a9c520a8de8545a923efc42ae159745
AcSsalazar <acsalazar@unal.edu.co>
2025-06-04 11:43:06 -0500

    only front branch

--- backend/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import predict, get_predictions
-
-urlpatterns = [
-    path('predict/', predict, name='predict'),
-    path('predictions/', get_predictions, name='get_predictions'),
-]
